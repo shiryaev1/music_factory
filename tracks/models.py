c270486f3e32ccdd3ec7a33f1e2ac355776fa2ab
Andreu Maleiko <amaleiko69@mail.ru>
2020-02-03 22:53:51 +0300

    models were created(tracks)

--- tracks/models.py
+++ tracks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Track(models.Model):
+    title = models.CharField(max_length=100)
+    singer = models.ForeignKey("Singer", on_delete=models.CASCADE)
+    genre = models.OneToOneField(Genre, on_delete=models.CASCADE)
+    music = models.FileField(upload_to='path/to/folder')
+
+    def __str__(self):
+        return self.title
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=50,blank=True)
+    surname = models.CharField(max_length=50,blank=True)
+    nickname = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.nickname
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
