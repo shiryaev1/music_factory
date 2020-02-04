1506a2426a1514b50952886dcae108be0f4d2986
Nikita <shiryaevv1@yandex.by>
2020-02-04 20:30:00 +0300

    fix upload_tracks

--- tracks/models.py
+++ tracks/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Track(models.Model):
     title = models.CharField(max_length=100)
     singer = models.ForeignKey('Singer', on_delete=models.CASCADE)
-    genre = models.OneToOneField('Genre', on_delete=models.CASCADE)
-    music = models.FileField(upload_to='static/media/upload_tracks/')
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    music = models.FileField(upload_to='upload_tracks/')
 
     def __str__(self):
         return self.title
